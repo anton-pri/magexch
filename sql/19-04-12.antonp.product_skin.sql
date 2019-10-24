e1c69e767d8fd707d1fe81c25c54fe6bfc4137d5
Anton Pribytov <antonpribytov@gmail.com>
2019-10-24 02:13:48 -0400

    FIX: custom skin folder full path added

--- sql/19-04-12.antonp.product_skin.sql
+++ sql/19-04-12.antonp.product_skin.sql
@@ -4,7 +4,7 @@ select @aid:=attribute_id from cw_attributes where field='magexch_product_skin';
 
 insert into cw_attributes_default (value, attribute_id, is_default, active) values ('', @aid, 1, 1);
 
-insert into cw_attributes_default (value, attribute_id, is_default, active) values ('ImageDoubleWidth', @aid, 0, 1);
+insert into cw_attributes_default (value, attribute_id, is_default, active) values ('/skins_custom_products/ImageDoubleWidth', @aid, 0, 1);
 
 -- insert into cw_attributes_default (value, attribute_id, is_default, active) values ('', @aid, 1, 1), ('/skins_custom_products/test1', @aid, 0, 1), ('/skins_custom_products/test2', @aid, 0, 1), ('/skins_custom_products/test3', @aid, 0, 1);
 
